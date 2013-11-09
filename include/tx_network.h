7a13a08b577fb9fb428fdc741ddc4c5011fa0c24
guoxing.pei <guoxing.pei@tct-nj.com>
2013-11-09 15:25:18 +0800

    let netinet file support compile

---
+++ include/tx_network.h
@@ -0,0 +1,18 @@
+#ifndef _TX_NETWORK_H_
+#define _TX_NETWORK_H_
+
+#include <stdio.h>
+#include <errno.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <tx_queue.h>
+
+#define VNET(var) V_##var
+#define VNET_DEFINE(type, var) type V_##var
+#define VNET_DECLARE(type, var) extern type V_##var
+#define VNET_PCPUSTAT_DECLARE(type, name) 
+
+extern int ticks;
+#define ERTT_NEW_MEASUREMENT            0x01
+
+#endif
