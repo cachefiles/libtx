11ec9321bd3b8331f3b930215e2477b7d6ac9dfe
pagxir <pagxir@gmail.com>
2013-11-11 03:42:01 +0800

    all cc file could be compiable

--- include/tx_network.h
+++ include/tx_network.h
@@ -5,6 +5,7 @@
 #include <errno.h>
 #include <stdint.h>
 #include <limits.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <tx_queue.h>
 
@@ -12,6 +13,18 @@
 #define VNET_DEFINE(type, var) type V_##var
 #define VNET_DECLARE(type, var) extern type V_##var
 #define VNET_PCPUSTAT_DECLARE(type, name) 
+#define MALLOC_DEFINE(a, b, c)
+#define VNET_ITERATOR_DECL(v)
+#define VNET_FOREACH(v)
+#define CURVNET_SET(v)
+
+#define uma_zalloc(s, t) malloc(s)
+#define uma_zfree(p, t) free(p)
+typedef struct uma_zone_s * uma_zone_t;
+
+#define malloc(p, args...) malloc(p)
+#define free(p, args...) free(p)
+#define __unused
 
 extern int hz;
 extern int ticks;
