c9f340437f7bdebb82a512b3c464e198017074e7
pagxir <pagxir@gmail.com>
2013-11-11 03:42:01 +0800

    all cc file could be compiable

--- TXNETWORK.mk
+++ TXNETWORK.mk
@@ -1,6 +1,28 @@
 CFLAGS += -I. -Iinclude
 
-all:
-	$(CC) -c $(CFLAGS) netinet/cc/cc_cubic.c
-	$(CC) -c $(CFLAGS) netinet/cc/cc_htcp.c
-	$(CC) -c $(CFLAGS) netinet/cc/cc_newreno.c
+all: cc_cubic.o cc_htcp.o cc_newreno.o cc_vegas.o cc_hd.o cc_chd.o cc_cdg.o cc.o
+
+cc_htcp.o: netinet/cc/cc_htcp.c
+	$(CC) -c $(CFLAGS) $^ 
+
+cc_vegas.o: netinet/cc/cc_vegas.c
+	$(CC) -c $(CFLAGS) $^ 
+
+cc_cubic.o: netinet/cc/cc_cubic.c
+	$(CC) -c $(CFLAGS) $^ 
+
+cc_newreno.o: netinet/cc/cc_newreno.c
+	$(CC) -c $(CFLAGS) $^ 
+
+cc.o: netinet/cc/cc.c
+	$(CC) -c $(CFLAGS) $^ 
+
+cc_cdg.o: netinet/cc/cc_cdg.c
+	$(CC) -c $(CFLAGS) $^ 
+
+cc_chd.o: netinet/cc/cc_chd.c
+	$(CC) -c $(CFLAGS) $^ 
+
+cc_hd.o: netinet/cc/cc_hd.c
+	$(CC) -c $(CFLAGS) $^ 
+
