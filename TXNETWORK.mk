3a51947fa71788dab97c1b35369e70242e360bf7
pagxir <pagxir@gmail.com>
2013-11-19 08:38:22 +0800

    let compiable for tcp_debug

--- TXNETWORK.mk
+++ TXNETWORK.mk
@@ -1,6 +1,6 @@
 CFLAGS += -I. -Iinclude
 
-all: cc_cubic.o cc_htcp.o cc_newreno.o cc_vegas.o cc_hd.o cc_chd.o cc_cdg.o cc.o
+all: cc_cubic.o cc_htcp.o cc_newreno.o cc_vegas.o cc_hd.o cc_chd.o cc_cdg.o cc.o tcp_debug.o
 
 cc_htcp.o: netinet/cc/cc_htcp.c
 	$(CC) -c $(CFLAGS) $^ 
@@ -26,3 +26,33 @@ cc_chd.o: netinet/cc/cc_chd.c
 cc_hd.o: netinet/cc/cc_hd.c
 	$(CC) -c $(CFLAGS) $^ 
 
+tcp_debug.o: netinet/tcp_debug.c
+	$(CC) -c $(CFLAGS) $^ 
+
+# tcp_debug.c      tcp_lro.c      tcp_reass.c  tcp_syncache.c  tcp_usrreq.c
+# tcp_hostcache.c  tcp_offload.c  tcp_sack.c   tcp_timer.c
+# tcp_input.c      tcp_output.c   tcp_subr.c   tcp_timewait.c
+
+tcp_reass.o: netinet/tcp_reass.c
+	$(CC) -c $(CFLAGS) $^ 
+
+tcp_usrreq.o: netinet/tcp_usrreq.c
+	$(CC) -c $(CFLAGS) $^ 
+
+tcp_sack.o: netinet/tcp_sack.c
+	$(CC) -c $(CFLAGS) $^ 
+
+tcp_timer.o: netinet/tcp_timer.c
+	$(CC) -c $(CFLAGS) $^ 
+
+tcp_input.o: netinet/tcp_input.c
+	$(CC) -c $(CFLAGS) $^ 
+
+tcp_output.o: netinet/tcp_output.c
+	$(CC) -c $(CFLAGS) $^ 
+
+tcp_subr.o: netinet/tcp_subr.c
+	$(CC) -c $(CFLAGS) $^ 
+
+tcp_timewait.o: netinet/tcp_timewait.c
+	$(CC) -c $(CFLAGS) $^ 
