ee0a153e0a31940cd6a9fdd2c20d7e470fce60a7
level <pagxir@gmail.com>
2014-11-04 13:23:36 +0800

    finish relay test

--- txrelay.cpp
+++ txrelay.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <stdlib.h>
 
 #ifdef WIN32
 #include <windows.h>
@@ -59,10 +60,21 @@ static void update_timer(void *up)
 	return;
 }
 
+#define FLAG_UPLOAD     0x01
+#define FLAG_DOWNLOAD   0x02
+#define FLAG_CONNECTING 0x04
+
 struct channel_context {
+	int flags;
 	tx_aiocb file;
 	tx_aiocb remote;
 	tx_task_t task;
+
+	int upl, upo;
+	char upbuf[8192];
+
+	int downl, downo;
+	char downbuf[8192];
 };
 
 static void do_channel_release(struct channel_context *up)
@@ -82,32 +94,174 @@ static void do_channel_release(struct channel_context *up)
 static int do_channel_poll(struct channel_context *up)
 {
 	int count;
-	char buf[8192];
+	char *pbuf;
+	int change = 0;
 	tx_aiocb *cb = &up->file;
 
-	if (tx_writable(&up->remote)) {
-		fprintf(stderr, "connect is ok\n");
+	if (!tx_writable(&up->remote) &&
+			(up->flags & FLAG_CONNECTING)) {
+		fprintf(stderr, "waiting connect...");
+		return 0;
+	} else {
+		up->flags &= ~FLAG_CONNECTING;
 	}
 
-	while (tx_readable(cb)) {
-		count = recv(cb->tx_fd, buf, sizeof(buf), 0);
-		tx_aincb_update(cb, count);
+	do {
+		change = 0;
 
-		if (!tx_readable(cb)) {
-			tx_aincb_active(cb, &up->task);
-			break;
+		cb = &up->file;
+		pbuf = up->upbuf + up->upl;
+		while (tx_readable(cb) && up->upl < sizeof(up->upbuf)) {
+			count = recv(cb->tx_fd, pbuf, sizeof(up->upbuf) - up->upl, 0);
+			tx_aincb_update(cb, count);
+
+			if (!tx_readable(cb)) {
+				break;
+			}
+
+			if (count == 0) {
+				up->flags &= ~FLAG_UPLOAD;
+				break;
+			}
+
+			if (count == -1) {
+				fprintf(stderr, "enter up/in error finish\n");
+				return -1;
+			}
+
+			up->upl += count;
+			pbuf += count;
 		}
 
-		if (count == 0 || count == -1) {
-			fprintf(stderr, "enter finish\n");
-			return -1;
+		cb = &up->remote;
+		pbuf = up->upbuf + up->upo;
+		while (tx_writable(cb) && up->upo < up->upl) {
+			count = tx_outcb_write(cb, pbuf, up->upl - up->upo);
+			if (count > 0) {
+				up->upo += count;
+				pbuf += count;
+				change = 1;
+			}
+
+			if (!tx_writable(cb)) {
+				break;
+			}
+
+			if (count == -1 && tx_writable(cb)) {
+				fprintf(stderr, "enter up/out error finish\n");
+				return -1;
+			}
+
+			change = 1;
 		}
 
-		fprintf(stderr, "get block: %d\n", count);
+		if (up->upo == up->upl) {
+			up->upl = 0;
+			up->upo = 0;
+		}
+
+	} while (change);
+
+	do {
+		change = 0;
+
+		cb = &up->remote;
+		pbuf = up->downbuf + up->downl;
+		while (tx_readable(cb) && up->downl < sizeof(up->downbuf)) {
+			count = recv(cb->tx_fd, pbuf, sizeof(up->downbuf) - up->downl, 0);
+			tx_aincb_update(cb, count);
+
+			if (!tx_readable(cb)) {
+				break;
+			}
+
+			if (count == 0) {
+				up->flags &= ~FLAG_DOWNLOAD;
+				break;
+			}
+
+			if (count == -1) {
+				fprintf(stderr, "enter down/in error finish\n");
+				return -1;
+			}
+
+			up->downl += count;
+			pbuf += count;
+		}
+
+		cb = &up->file;
+		pbuf = up->downbuf + up->downo;
+		while (tx_writable(cb) && up->downo < up->downl) {
+			count = tx_outcb_write(cb, pbuf, up->downl - up->downo);
+			if (count > 0) {
+				up->downo += count;
+				pbuf += count;
+				change = 1;
+			}
+
+			if (!tx_writable(cb)) {
+				break;
+			}
+
+			if (count == -1 && tx_writable(cb)) {
+				fprintf(stderr, "enter down/out error finish\n");
+				return -1;
+			}
+
+			change = 1;
+		}
+
+		if (up->downo == up->downl) {
+			up->downo = 0;
+			up->downl = 0;
+		}
+
+	} while (change);
+
+	if (up->downo == up->downl &&
+		up->upl == up->upo && 0 == (up->flags & (FLAG_UPLOAD| FLAG_DOWNLOAD))) {
+		fprintf(stderr, "nomalize finish\n");
+		return -1;
 	}
 
-	fprintf(stderr, "return\n");
-	tx_aincb_active(cb, &up->task);
+	cb = &up->file;
+	if (!tx_readable(cb) && up->upl < sizeof(up->upbuf)) {
+		//fprintf(stderr, "upload wait read .. %p\n", cb);
+		tx_aincb_active(cb, &up->task);
+	}
+
+	if (!tx_writable(cb) && up->downo < up->downl) {
+		//fprintf(stderr, "download wait write .. %p\n", cb);
+		tx_outcb_prepare(cb, &up->task, 0);
+	}
+
+	if ((up->flags & FLAG_DOWNLOAD) == 0 && up->downo == up->downl) {
+		//fprintf(stderr, "shutdown download: %d\n", cb->tx_fd);
+		shutdown(cb->tx_fd, SHUT_WR);
+	}
+
+	cb = &up->remote;
+	if (!tx_readable(cb) && up->downl < sizeof(up->downbuf)) {
+		//fprintf(stderr, "download wait read .. %p\n", cb);
+		tx_aincb_active(cb, &up->task);
+	}
+
+	if (!tx_writable(cb) && up->upo < up->upl) {
+		//fprintf(stderr, "upload wait write .. %p\n", cb);
+		tx_outcb_prepare(cb, &up->task, 0);
+	}
+
+	if ((up->flags & FLAG_UPLOAD) == 0 && up->upl == up->upo) {
+		//fprintf(stderr, "shutdown upload: %d\n", cb->tx_fd);
+		shutdown(cb->tx_fd, SHUT_WR);
+	}
+
+#if 0
+	fprintf(stderr, "%p upstat %d %d r%x w%x\n",
+			&up->remote, up->upo, up->upl, tx_readable(&up->file), tx_writable(&up->remote));
+	fprintf(stderr, "%p downstat %d %d r%x w%x\n",
+			&up->file, up->downo, up->downl, tx_readable(&up->remote), tx_writable(&up->file));
+#endif
 	return 0;
 }
 
@@ -116,7 +270,6 @@ static void do_channel_wrapper(void *up)
 	int err;
 	struct channel_context *upp;
 
-	fprintf(stderr, "do_channel_wrapper\n");
 	upp = (struct channel_context *)up;
 	err = do_channel_poll(upp);
 
@@ -127,6 +280,9 @@ static void do_channel_wrapper(void *up)
 
 	return;
 }
+
+static int target_port = 30007;
+static const char *target_host = "127.0.0.1";
 
 static void do_channel_prepare(struct channel_context *up, int newfd)
 {
@@ -150,9 +306,13 @@ static void do_channel_prepare(struct channel_context *up, int newfd)
 	tx_setblockopt(peerfd, 0);
 	tx_aiocb_init(&up->remote, loop, peerfd);
 	sin0.sin_family = AF_INET;
-	sin0.sin_port   = htons(80);
-	sin0.sin_addr.s_addr = inet_addr("180.97.33.108");
+	sin0.sin_port   = htons(target_port);
+	sin0.sin_addr.s_addr = inet_addr(target_host);
 	tx_aiocb_connect(&up->remote, (struct sockaddr *)&sin0, &up->task);
+
+	up->upl = up->upo = 0;
+	up->downl = up->downo = 0;
+	up->flags = (FLAG_UPLOAD| FLAG_DOWNLOAD| FLAG_CONNECTING);
 
 	fprintf(stderr, "newfd: %d to here\n", newfd);
 	return;
@@ -187,16 +347,19 @@ static void do_listen_accepted(void *up)
 	return;
 }
 
-static void init_listen(struct listen_context *up)
+static void listen_init(struct listen_context *up)
 {
 	int fd;
 	int err;
+	int option = 1;
 	tx_loop_t *loop;
 	struct sockaddr_in sa0;
 
 	fd = socket(AF_INET, SOCK_STREAM, 0);
 
 	tx_setblockopt(fd, 0);
+
+	setsockopt(fd,SOL_SOCKET, SO_REUSEADDR, (char*)&option,sizeof(option));
 
 	sa0.sin_family = AF_INET;
 	sa0.sin_port   = htons(30008);
@@ -223,6 +386,9 @@ int main(int argc, char *argv[])
 	struct uptick_task uptick;
 	struct listen_context listen0;
 
+	if (argc > 1) target_host = argv[1];
+	if (argc > 2) target_port = atoi(argv[2]);
+
 	unsigned int last_tick = 0;
 	tx_loop_t *loop = tx_loop_default();
 	tx_poll_t *poll = tx_epoll_init(loop);
@@ -238,7 +404,7 @@ int main(int argc, char *argv[])
 	tx_task_init(&tmtask.task, loop, update_timer, &tmtask);
 	tx_timer_reset(&tmtask.timer, 500);
 
-	init_listen(&listen0);
+	listen_init(&listen0);
 
 	tx_loop_main(loop);
 
