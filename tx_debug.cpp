7b43f33bbfd87c30bcd9891ef2c1fcb68c3e9bfa
pagxir <pagxir@gmail.com>
2017-01-02 11:10:43 +0800

    change debug helper untils api

--- tx_debug.cpp
+++ tx_debug.cpp
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
 
 #include "tx_debug.h"
+
+const char * get_debug_format(const char *format)
+{
+	struct tm *tmp;
+	struct timeval tv;
+	static char long_format[8192];
+
+	time_t now;
+	gettimeofday(&tv, NULL);
+
+	now = tv.tv_sec;
+	tmp = localtime(&now);
+	snprintf(long_format, sizeof(long_format),
+			"%02d:%02d:%02d.%03d %s",
+			tmp->tm_hour, tmp->tm_min, tmp->tm_sec, tv.tv_usec/1000, format);
+	return long_format;
+}
 
 void __tx_check__(int cond, const char *msg, int line, const char *file)
 {
