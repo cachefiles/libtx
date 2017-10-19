8930d03e45ac1841d73bfd691d63f6cc9d9d3fb4
pagxir <pagxir@gmail.com>
2017-10-19 21:19:19 +0800

    add LOGABLE_TAGS to config log tags

--- tx_debug.cpp
+++ tx_debug.cpp
@@ -182,9 +182,21 @@ static FILE *log_get(int fd, const char *tag)
 	return _logfp;
 }
 
-static int log_logable(const char *tag)
+static const char *_logable_tags = NULL;
+static const char *default_logable_tags = "FEWIDV";
+
+static int log_logable(const char *logtag)
 {
-	return 1;
+	if (_logable_tags == NULL) {
+		_logable_tags = getenv("LOGABLE_TAGS");
+
+		if (_logable_tags == NULL) {
+			_logable_tags = default_logable_tags;
+			assert (_logable_tags != NULL);
+		}
+	}
+
+	return (logtag == NULL) || (NULL != strstr(_logable_tags, logtag));
 }
 
 static int log_put(FILE *log)
